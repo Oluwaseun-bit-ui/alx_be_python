4705666b8f2d1bd8ffe2c565380ca4e777afc5b3
oluwaseun <t.oluwaseunofficial@gmail.com>
2025-05-31 14:06:18 -0400

    Add simple_interest.py to calculate simple interest

---
+++ python_introduction/rectangle_calculations.py
@@ -0,0 +1,10 @@
+length = 10
+width = 5
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area of rectangle with length", length, "and width", width, "is", area)
+print("perimeter of rectangle with length", length, "and width", width,"is", perimeter
+)
+
